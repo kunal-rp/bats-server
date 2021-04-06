bba2fd703c0fa55d829b6671de76a14ae8bee583
Ethan Wright <wrightet707@gmail.com>
2021-04-05 22:10:01 -0700

    add classes for email fields when valid

--- public/login_validations.js
+++ public/login_validations.js
@@ -2,8 +2,19 @@ let login_btn = document.getElementById("login_submit");
 login_btn.addEventListener("click",checkPassword);
 function checkPassword(){
     let password = document.getElementById("password");
-    console.log(password.innerHTML);
     if(password.innerHTML === ""){
         password.classList.add('password_error');
+    }
+}
+
+let email = document.getElementById("email");
+let email_label = document.getElementById("email-label");
+email.addEventListener("click", checkEmail);
+email_label.addEventListener("click", checkEmail);
+function checkEmail(){
+    console.log(email.innerHTML === '')
+    if(email.innerHTML !== ""){
+        email.classList.add('email_populated');
+        email_label.classList.add('email_label_valid');
     }
 }
