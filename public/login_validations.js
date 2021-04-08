f5273d6e4cf4c575521a05430bf408d4db59fca2
Ethan Wright <wrightet707@gmail.com>
2021-04-07 18:28:48 -0700

    make sure both errors do not render at once

--- public/login_validations.js
+++ public/login_validations.js
@@ -1,10 +1,12 @@
 let login_btn = document.getElementById("login_submit");
-login_btn.addEventListener("click",checkPassword);
 let password_error_message = document.getElementById('password_error_message');
 let password_message_success = document.getElementById('password_message_success');
 let password_label = document.getElementById('password-label');
 let password = document.getElementById("password");
+
 password.addEventListener('input', checkPassword);
+login_btn.addEventListener("click", checkPassword);
+
 function checkPassword(){
     
     if(password.value === ""){
@@ -16,6 +18,8 @@ function checkPassword(){
     if(password.value !== ""){
         password.classList.add('success');
         password_message_success.classList.remove('password_message');
+        password_error_message.classList.remove('password_message_error');
+        password_error_message.classList.add('password_message');
         password_message_success.classList.add('password_message_success');
         password_label.classList.add('password-label');
 
